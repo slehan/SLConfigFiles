48cd8da66f714a85ae33339497f06b46595e539d
test <test@tests-MacBook-Pro.local>
2015-09-28 10:08:14 -0400

    now installs cmake and tmux, need to retry on fresh install again

--- install-osx.sh
+++ install-osx.sh
@@ -10,6 +10,8 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 # VIM
 echo "--- Installing Vim configuration... ---"
 brew install vim
+brew install cmake
+brew install tmux
 echo "Installing Vundle..."
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -s $SCRIPT_DIR/vim/.vimrc ~/.vimrc 
@@ -17,7 +19,7 @@ echo "Installing YCM for Vim..."
 echo "For YCM, making sure cmake and python are installed..."
 echo "Linking libclang.so.1 to libclang.so..."
 vim +PluginInstall +qall
-sudo ln -s /usr/lib/llvm-3.5/lib/libclang.so.1 /usr/lib/llvm-3.5/lib/libclang.so
+#sudo ln -s /usr/lib/llvm-3.5/lib/libclang.so.1 /usr/lib/llvm-3.5/lib/libclang.so
 ~/.vim/bundle/YouCompleteMe/install.py --clang-completer --system-libclang
 
 
