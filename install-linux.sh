ebe684b5c12abf7a54e3140c6283f2d7ba8e3e10
Skyler Lehan <slehan117@icloud.com>
2015-09-28 16:55:47 -0400

    Script now colors the status messages correctly

--- install-linux.sh
+++ install-linux.sh
@@ -10,21 +10,21 @@ echo "Enter username: "
 read USERNAME
 
 # VIM
-echo "${RED}--- Installing Vim configuration... ---${NO_COLOR}"
+echo -e "${RED}--- Installing Vim configuration... ---${NO_COLOR}"
 sudo apt-get install vim-nox llvm clang tmux build-essential cmake python-dev
-echo "${RED}Installing Vundle...{$NO_COLOR}"
+echo -e "${RED}Installing Vundle...{$NO_COLOR}"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -s $SCRIPT_DIR/vim/.vimrc ~/.vimrc 
-echo "${RED}Installing YCM for Vim...${NO_COLOR}"
-echo "${RED}For YCM, making sure cmake and python are installed...${NO_COLOR}"
-echo "${RED}Linking libclang.so.1 to libclang.so...${NO_COLOR}"
+echo -e "${RED}Installing YCM for Vim...${NO_COLOR}"
+echo -e "${RED}For YCM, making sure cmake and python are installed...${NO_COLOR}"
+echo -e "${RED}Linking libclang.so.1 to libclang.so...${NO_COLOR}"
 vim +PluginInstall +qall
 sudo ln -s /usr/lib/llvm-3.5/lib/libclang.so.1 /usr/lib/llvm-3.5/lib/libclang.so
 ~/.vim/bundle/YouCompleteMe/install.py --clang-completer --system-libclang
 
 
 # tmux
-echo "${RED}--- Installing tmux configuration... ---${NO_COLOR}"
+echo -e "${RED}--- Installing tmux configuration... ---${NO_COLOR}"
 ln -s $SCRIPT_DIR/tmux/.tmux.conf ~/.tmux.conf
 tmux source ~/.tmux.conf
 
