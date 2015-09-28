1dae7e2eac350b76ffaa61d6de09b38736ab4a37
Skyler Lehan <slehan117@icloud.com>
2015-09-28 08:30:04 -0400

    Modified the script to install vim-nox so that it works with Python, also doing PluginInstall before completely installing YCM

--- install-linux.sh
+++ install-linux.sh
@@ -5,16 +5,17 @@ SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 # VIM
 echo "--- Installing Vim configuration... ---"
-sudo apt-get install vim llvm tmux build-essential cmake python-dev
+sudo apt-get install vim-nox llvm tmux build-essential cmake python-dev
 echo "Installing Vundle..."
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -s $SCRIPT_DIR/vim/.vimrc ~/.vimrc 
 echo "Installing YCM for Vim..."
 echo "For YCM, making sure cmake and python are installed..."
 echo "Linking libclang.so.1 to libclang.so..."
+vim +PluginInstall +qall
 sudo ln -s /usr/lib/llvm-3.5/lib/libclang.so.1 /usr/lib/llvm-3.5/lib/libclang.so
 ~/.vim/bundle/YouCompleteMe/install.py --clang-completer --system-libclang
-vim +PluginInstall +qall
+
 
 # tmux
 echo "--- Installing tmux configuration... ---"
