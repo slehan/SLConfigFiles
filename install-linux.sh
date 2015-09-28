d67d964668db509702b5061b10278a5cbd18031d
Skyler Lehan <slehan117@icloud.com>
2015-09-28 08:20:23 -0400

    Modified the install script so that it also makes sure vim, tmux, cmake, llvm and build-essential are on the latest

--- install-linux.sh
+++ install-linux.sh
@@ -5,13 +5,12 @@ SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 # VIM
 echo "--- Installing Vim configuration... ---"
+sudo apt-get install vim llvm tmux build-essential cmake python-dev
 echo "Installing Vundle..."
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -s $SCRIPT_DIR/vim/.vimrc ~/.vimrc 
 echo "Installing YCM for Vim..."
 echo "For YCM, making sure cmake and python are installed..."
-sudo apt-get install build-essential cmake
-sudo apt-get install python-dev
 echo "Linking libclang.so.1 to libclang.so..."
 sudo ln -s /usr/lib/llvm-3.5/lib/libclang.so.1 /usr/lib/llvm-3.5/lib/libclang.so
 ~/.vim/bundle/YouCompleteMe/install.py --clang-completer --system-libclang
